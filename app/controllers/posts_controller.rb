cee8b1faa8323a3a186181a9c31488b473764c17
yzrh42 <aobwest@gmail.com>
2025-06-05 00:04:08 +0900

    投稿一覧のviewを編集

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -46,6 +46,6 @@ class PostsController < ApplicationController
     private
     
      def post_params
-        params.require(:post).permit(:user_id, :text, :date, :image)
+        params.require(:post).permit(:user_id, :body, :date, :image)
     end
 end
